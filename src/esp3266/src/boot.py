3582aa554ff899285e7e37cc1dc6f0fb31590ead
SteveMayze <smayze@yahoo.com>
2022-06-25 23:56:37 +0200

    Adds the REST capability via the EPP8266

---
+++ src/esp3266/src/boot.py
@@ -0,0 +1,38 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+
+try:
+    import usocket as socket
+except:
+    import socket
+
+import uos, machine, sys
+from machine import Pin
+from machine import UART
+
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+import webrepl
+import network
+import json
+
+webrepl.start()
+gc.collect()
+
+ESSID = "WN0463FB2"
+PASSWORD = "53240958296039385782"
+ 
+def connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ESSID, PASSWORD)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+ 
+connect()
+
+led = Pin(2, Pin.OUT)
---
+++ src/esp3266/src/main.py
@@ -0,0 +1,188 @@
+### D7 RXD2
+### D8 RXD2
+
+### from machine import UART
+
+# Complete project details at https://RandomNerdTutorials.com
+
+
+def web_page():
+  html = 'success'
+  return html
+
+
+def byteToInt(byte):
+    """
+    byte -> int
+
+    Determines whether to use ord() or not to get a byte's value.
+    """
+    if hasattr(byte, 'bit_length'):
+        # This is already an int
+        return byte
+    return ord(byte) if hasattr(byte, 'encode') else byte[0]
+
+def intToByte(i):
+    """
+    int -> byte
+
+    Determines whether to use chr() or bytes() to return a bytes object.
+    """
+    return chr(i) if hasattr(bytearray(), 'encode') else bytearray([i])
+
+def stringToBytes(s):
+    """
+    string -> bytes
+
+    Converts a string into an appropriate bytes object
+    """
+    return s.encode('ascii') if sys.version_info >= (3, 0) else s
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+uart = UART(1, baudrate=9600, bits=8, parity=None, stop=1)
+
+while True:
+  print('Waiting for a connection')
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+
+  bodyIdx = request.find('\\r\\n\\r\\n') + 8
+  body = request[bodyIdx:-1]
+
+  print('body: %s ' % body)
+
+  jbody = json.loads(body)
+  action = jbody['action']
+  if action == 'message':
+    text = jbody['param']
+    data_length = len(text)+1
+    frame = bytearray()
+    frame.append(0xFE)
+    frame.append(data_length)
+    frame.append(0x01)
+    frame += stringToBytes(text)
+
+    chksum = 0
+    for bb in frame[2:]:
+      chksum += bb
+    chksum = chksum & 0xFF
+    chksum = intToByte( 0xFF - chksum)
+
+    frame += chksum
+
+    print('Frame: %s ' % frame)
+    sent = uart.write(frame)
+    print("Sent %d" % sent)
+
+  if action == 'colour':
+    colour = jbody['param']
+
+    frame = bytearray()
+    frame.append(0xFE)
+    frame.append(0x02)
+    frame.append(0x02)
+    if colour == 'red':
+      frame.append(0x01)
+    elif colour == 'orange':
+      frame.append(0x01)
+    elif colour == 'yellow':
+      frame.append(0x01)
+    elif colour == 'green':
+      frame.append(0x02)
+    elif colour == 'blue':
+      frame.append(0x03)
+    elif colour == 'indigo':
+      frame.append(0x03)
+    elif colour == 'violate':
+      frame.append(0x03)
+    else:
+      frame.append(0x01)
+
+    chksum = 0
+    for bb in frame[2:]:
+      chksum += bb
+    chksum = chksum & 0xFF
+    chksum = intToByte( 0xFF - chksum)
+
+    frame += chksum
+
+    print('Frame: %s ' % frame)
+    sent = uart.write(frame)
+    print("Sent %d" % sent)
+
+  if action == 'speed':
+    speed = jbody['param']
+
+    frame = bytearray()
+    frame.append(0xFE)
+    frame.append(0x02)
+    frame.append(0x03)
+    if speed == 'stop':
+      frame.append(0x00)
+    elif speed == 'start':
+      frame.append(0x7F)
+    else:
+      frame.append(0x7F)
+    
+
+    chksum = 0
+    for bb in frame[2:]:
+      chksum += bb
+    chksum = chksum & 0xFF
+    chksum = intToByte( 0xFF - chksum)
+
+    frame += chksum
+
+    print('Frame: %s ' % frame)
+    sent = uart.write(frame)
+    print("Sent %d" % sent)
+
+  if action == 'reset':
+    frame = bytearray()
+    frame.append(0xFE)
+    frame.append(0x01)
+    frame.append(0x04)
+
+    chksum = 0
+    for bb in frame[2:]:
+      chksum += bb
+    chksum = chksum & 0xFF
+    chksum = intToByte( 0xFF - chksum)
+
+    frame += chksum
+
+    print('Frame: %s ' % frame)
+    sent = uart.write(frame)
+    print("Sent %d" % sent)
+
+  if sent > 0:
+    ## uart_response = uart.read()
+    uart_response = 'void'
+    if uart_response == None:
+      response = 'error - send to UART failed - timeout'
+      conn.send('HTTP/1.1 405 OK\n')
+      conn.send('Content-Type: text/plain\n')
+      conn.send('Connection: close\n\n')
+      conn.sendall(response)
+    else:
+      response = 'success %s' % uart_response
+      conn.send('HTTP/1.1 200 OK\n')
+      conn.send('Content-Type: text/plain\n')
+      conn.send('Connection: close\n\n')
+      conn.sendall(response)
+  else:
+    response = 'error - send to UART failed.'
+    conn.send('HTTP/1.1 405 OK\n')
+    conn.send('Content-Type: text/plain\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+
+  conn.close()
+
